de7726b9d7c8bed1a95737ded638903e0c1dfefc
Doogie187 <9jzvv9n2wp@privaterelay.appleid.com>
2026-09-04 15:01:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'doogie-v42';
+const CACHE = 'doogie-v43';
 const ASSETS = ['./','./index.html','./background.jpg','./manifest.webmanifest','./apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
